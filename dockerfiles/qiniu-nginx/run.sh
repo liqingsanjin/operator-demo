efbe92b63791d619ff1cb8c31f1f007c3b76f2a8
admin <admin@admindeMacBook-Pro.local>
2017-11-01 15:23:33 +0800

    fix sh

--- dockerfiles/qiniu-nginx/run.sh
+++ dockerfiles/qiniu-nginx/run.sh
@@ -2,17 +2,19 @@
 
 image="liqingsanjin/jingx:v$1"
 
+echo $image
+
 cd $GOPATH/src/operator-demo/dockerfiles/qiniu-nginx/
 
 go build -o app $GOPATH/src/operator-demo/cmd/k8s/qiniu_nginx.go
 
 echo "build go app success"
 
-docker build -t image .
+docker build -t $image .
 
 echo "build docker image success"
 
-docker push image
+docker push $image
 
 kubectl create -f $GOPATH/src/operator-demo/yaml/demo/qiniu_nginx.yaml
 
